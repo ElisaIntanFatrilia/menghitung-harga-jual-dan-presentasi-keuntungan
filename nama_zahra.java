975ebfa21212e85089878fe42e2bffbb7f6bb68e
Azzahra4G <zahrazahra030202@gmail.com>
2023-06-04 17:02:05 +0700

    zahra

---
+++ nama_zahra.java
@@ -0,0 +1,6 @@
+public class nama_zahra{
+    public static void main(String[] args) {
+        System.out.println("Nama Saya : Az-Zahra Nur Agni Nabilah");
+        System.out.println("NIM : 2113010263");
+    }
+}
