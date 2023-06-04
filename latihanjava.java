ec169086acb1633b0e81d117973ff71d8a0304bf
ElisaIntanFatrilia <elisaintan188@gmail.com>
2023-06-04 15:55:58 +0700

    project

---
+++ latihanjava.java
@@ -0,0 +1,6 @@
+public class latihanjava{
+    public static void main(String[] args) {
+        System.out.println("Nama Saya : Elisa Intan Fatrilia");
+        System.out.println("NIM : 2113010272");
+    }
+}
