259e3ccc0577482eabe7cc04badd71cb4411e0bf
CelvinCT <133625476+CelvinCT@users.noreply.github.com>
2023-06-05 21:56:28 +0700

    Add files via upload

---
+++ Nama_Celvin.java
@@ -0,0 +1,17 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+
+/**
+ *
+ * @author HP
+ */
+public class Nama_Celvin {
+    public static void main(String[] args) {
+        System.out.println("Nama Saya   : Celvin Collins Tan");
+        System.out.println("NIM         : 2113010280");
+        System.out.println("Kelas       : 4G");
+    }
+    
+}
